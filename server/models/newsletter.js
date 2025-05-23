5a275a364c312cc9a217b8b3a6097f97b4ba1b13
phantumcoder <phantum.coder.5150@gmail.com>
2025-05-23 18:03:20 -0500

    defined the newsletter model

---
+++ server/models/newsletter.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const newsletterSubscription = new Schema ({
+    email: {
+        type: String,
+        required: [true, 'email address is required'],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [
+            /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+            'please enter a valid email address'
+        ]
+    },
+    firstName: {
+        type: String,
+        required: [true, 'first name is required'],
+        trim: true,
+        maxlength: [50,'first name must be less than 50 characters']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'last name is required'],
+        trim: true,
+        maxlength: [50,'last name must be less than 50 characters']
+    },
+
+    consentGiven: {
+        type: Boolean,
+        required: [true, 'consent is required'],
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    },
+
+    source: {
+        type: String,
+        trim: true,
+        required: [true, 'source is required'],
+    }
+})
+
+module.exports = mongoose.model('NewsletterSubscription', newsletterSubscription);
